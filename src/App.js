f3a81fa23649942df27c25a4eba605511a5b34e6
akshayv1811 <akshayv1811@gmail.com>
2024-08-12 19:35:30 +0530

    Added static part Forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
   return <div className="app">
     <div className="sidebar">
       <FriendsList initialFriends={initialFriends} />
+      <FormAddFriend />
+      <Button>Add friend</Button>
     </div>
+    <FormSplitBill />
   </div>
 }
 
@@ -63,8 +66,56 @@ function Friend({ friend }) {
       </p>
     )}
 
-    <button className = "button">
-      Select
-    </button>
+    <Button > Select</Button>
+
   </li>
+}
+
+
+function Button({ children }) {
+  return (
+    <button className="button">
+      {children}
+    </button>
+  )
+}
+
+function FormAddFriend() {
+  return <form className="form-add-friend">
+    <label>👫 Friend name</label>
+    <input type="text" />
+
+    <label>🌅Image URL</label>
+    <input type="text" />
+
+    <Button>Add</Button>
+
+  </form>
+}
+
+
+function FormSplitBill() {
+  return <form className="form-split-bill">
+    <h2>Split a bill with X</h2>
+
+    <label>💰 Bill value</label>
+    <input type="text" />
+
+    <label>🙎‍♂️  Your Expense</label>
+    <input type="text" />
+
+    <label>👨🏼‍🤝‍👨🏼 X's expense</label>
+    <input type="text" disabled />
+
+    <label>🤑 Who is paying the bill?</label>
+    <select>
+      <option value="user">You</option>
+      <option value="friend">X</option>
+
+    </select>
+
+
+    <Button>Split bill</Button>
+
+  </form>
 }
